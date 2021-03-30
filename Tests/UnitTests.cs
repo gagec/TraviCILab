0fd450154484f5beccb61ff27fb22aca38b9aa46
gagec <54290870+gagec@users.noreply.github.com>
2021-03-30 12:10:41 -0500

    Update Unit Tests
    added tests for all functions

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -29,7 +29,96 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
+         public void Subtract_ValidCrowell()
+        {
+            Assert.AreEqual(3, Program.Subtract("9", "6"));
+            Assert.AreEqual(6, Program.Subtract("10", "4"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
+        }
 
+        [Test]
+        public void Subtract_InvalidCrowell()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_NullCrowell()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
         // Implement 3 tests per operation, following a similar pattern as above
+         public void Multiply_ValidCrowell()
+        {
+            Assert.AreEqual(100, Program.Multiply("10", "10"));
+            Assert.AreEqual(4, Program.Multiply("1", "4"));
+            Assert.AreEqual(-7, Program.Multiply("-1", "7"));
+        }
+
+        [Test]
+        public void Multiply_InvalidCrowell()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_NullCrowell()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+        
+         public void Divide_ValidCrowell()
+        {
+            Assert.AreEqual(5, Program.Divide("10", "2"));
+            Assert.AreEqual(6, Program.Divide("66", "11"));
+            Assert.AreEqual(2.5, Program.Divide("5", "2"));
+        }
+
+        [Test]
+        public void Divide_InvalidCrowell()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_NullCrowell()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+        
+         public void Power_ValidCrowell()
+        {
+            Assert.AreEqual(16, Program.Power("2", "4"));
+            Assert.AreEqual(125, Program.Power("5", "3"));
+            Assert.AreEqual(10, Program.Power("2", "8")); // destined to fail
+        }
+
+        [Test]
+        public void Power_InvalidCrowell()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_NullCrowell()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
